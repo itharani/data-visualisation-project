7c70490826cb4543dc5dccd33b1611fc10ad33ad
Tharani Prathaban <itharanie@gmail.com>
2024-10-24 19:51:06 +0800

    Add the css, html and js files

---
+++ vega_lite_vis.js
@@ -0,0 +1,76 @@
+var vg_1 = "graphs/map.vg.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_2 = "graphs/donut_assault.vg.json";
+
+vegaEmbed("#donut1", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_3 = "graphs/donut_property_crime.vg.json";
+
+vegaEmbed("#donut2", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_4 = "graphs/isotype_grid.vg.json";
+
+vegaEmbed("#isotype_grid", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_5 = "graphs/population_pyramid.vg.json"
+
+vegaEmbed("#population_pyramid", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_6 = "graphs/type_of_drugs_donut.vg.json"
+
+vegaEmbed("#type_of_drugs", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_7 = "graphs/type_of_labour.vg.json"
+
+vegaEmbed("#type_of_labour", vg_7).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_8 = "graphs/heatmap.vg.json"
+
+vegaEmbed("#type_of_drug_offense_by_age", vg_8).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_9 = "graphs/radial_plot.vg.json"
+
+vegaEmbed("#radial_plot", vg_9).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_10 = "graphs/bar_in_bar_chart.vg.json"
+
+vegaEmbed("#bar_in_bar_chart", vg_10).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_11 = "filter_graphs/map2.vg.json"
+
+vegaEmbed("#map2", vg_11).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+var vg_12 = "filter_graphs/donut_chart.vg.json"
+
+vegaEmbed("#donut_chart", vg_12).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view    
+}).catch(console.error);
+
+
+
+
+
